e90d2a142619dcd05e0586f9075e1af8dd0ae791
Sanskar <sanskarkumar85111@gmail.com>
2022-04-20 22:42:04 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 function App() {
-    return (<BrowserRouter>
+	return (
+		<HashRouter>
 			<Routes>
-				<Route path='/' element={<Home />}/>
-				<Route path='/login' element={<Login />}/>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
 			</Routes>
-		</BrowserRouter>);
+		</HashRouter>
+	);
 }
 export default App;
